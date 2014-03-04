671cba11cfa3a21efc34d723645e666d35d5d591
dailawa <dailawadi@walmartlabs.com>
2014-03-04 15:41:32 -0800

    Blog App based on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'cc13c4e7fd8715d7f4a21866e5206d97a2b434a833e6ea1ed57da8d3b14b6c1ffe3286a6f3bc4b686a83f6d3425808479fe58ee544dffb2598542f5c511a0d74'
